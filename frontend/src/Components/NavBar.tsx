7246caeebd2a9b42ef7be053aa490d52e1260619
suka <khiemnguyen.hye@gmail.com>
2025-08-03 03:37:37 +0700

    Add buttons and logout function to Navbar

--- frontend/src/Components/NavBar.tsx
+++ frontend/src/Components/NavBar.tsx
@@ -1,16 +1,30 @@
+import { Link } from 'react-router-dom'
+import { useAuthStore } from '../store/useAthStore'
+import { Power, Smile } from 'lucide-react'
+
 export const NavBar = () => {
+    const { logout, authUser } = useAuthStore()
+
     return (
-        <div className="navbar bg-base-100 shadow-sm sticky top-0 z-50">
+        <div className="navbar bg-base-100 shadow-sm">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <Link to="/" className="btn btn-ghost text-xl">
+                    Bubbly
+                </Link>
             </div>
             <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current">
-                        {' '}
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>{' '}
-                    </svg>
-                </button>
+                <ul className="menu menu-horizontal px-2">
+                    <li>
+                        <Link to="/settings">
+                            <Smile className="h-7 w-5" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/logout" onClick={logout}>
+                            <Power className="h-7 w-5" />
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </div>
     )
