3ade62d846cc1375b457972877349a27706261c6
suka <khiemnguyen.hye@gmail.com>
2025-08-05 20:05:50 +0700

    Remove comments

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -6,7 +6,6 @@ import cloudinary from '../lib/cloudinary.ts'
 
 export const signup = async (req: Request, res: Response) => {
     try {
-        // ✏️ Take in signup info and validate
         const { username, email, password } = req.body
         if (!username || !email || !password) {
             res.status(400).json({ message: '❌ Missing required fields.' })
@@ -24,64 +23,60 @@ export const signup = async (req: Request, res: Response) => {
             return
         }
 
-        // ✏️ Generating a unique salt and hasing out new password
         const salt: string = await bcrypt.genSalt(10)
         const hashedPassword: string = await bcrypt.hash(password, salt)
 
         const newUser = new User({
             username: username,
             email: email,
-            password: hashedPassword, // ✏️ Hashed password is stored
+            password: hashedPassword, 
         })
 
         if (newUser) {
             generateTokenAndSetCookie(newUser._id, res)
-            await newUser.save() // ✏️ New user is added
+            await newUser.save()
             res.status(201).json({
-                message: '✔️ New user has been created!',
+                message: 'Account created successfully!',
                 username: newUser.username,
                 email: newUser.email,
                 profilePicture: newUser.profilePicture,
             })
         } else {
-            res.status(400).json({ message: '❌ Invalid user data.' })
+            res.status(400).json({ message: 'Invalid user data.' })
         }
     } catch (error) {
         console.log('💢 Error in Signup controller:', error)
-        res.status(500).json({ error: '💢 Something is broken on our end.' })
+        res.status(500).json({ error: 'Something is broken on our end.' })
     }
 }
 
 export const login = async (req: Request, res: Response) => {
     try {
-        // ✏️ Take in login info
         const { username, password } = req.body
 
         const user = await User.findOne({ username })
-        // ✏️ Validate given password against hashed password
-        const isValidPassword = bcrypt.compare(password, user?.password || '')
+        const isValidPassword = await bcrypt.compare(password, user?.password || '')
 
         if (!user || !isValidPassword) {
-            res.status(400).json({ message: '❌ Incorrect username or password.' })
+            res.status(400).json({ message: 'Incorrect username or password.' })
             return
         }
-        // ✏️ Generate new token and set session cookie
         generateTokenAndSetCookie(user._id, res)
 
         res.status(200).json({
-            message: '✔️ User sucessfully logged in.',
+            message: 'Logged in successfully.',
             username: user.username,
+            email: user.email,
             profilePicture: user.profilePicture,
         })
     } catch (error) {
         console.log('💢 Error in Login controller:', error)
-        res.status(500).json({ error: '💢 Something is broken on our end.' })
+        res.status(500).json({ error: 'Something is broken on our end.' })
     }
 }
 
 export const logout = (req: Request, res: Response) => {
     try {
-        // ✏️ Replaces the current JWT with a blank one - essentially terminating it. Sets max age to 0.
         res.cookie('jwt', '', { 
             maxAge: 0,
             httpOnly: true,
@@ -97,7 +92,6 @@ export const logout = (req: Request, res: Response) => {
 
 export const updateProfile = async (req: any, res: Response) => {
     try {
-        // ✏️ Take in Profile picture
         const { profilePicture } = req.body
         const userId = req.user._id
 
@@ -106,9 +100,9 @@ export const updateProfile = async (req: any, res: Response) => {
             return
         }
 
-        const uploadResponse = await cloudinary.uploader.upload(profilePicture) // ✏️ Uploads Profile pic to Cloudinary
+        const uploadResponse = await cloudinary.uploader.upload(profilePicture) 
 
-        const updatedUser = await User.findByIdAndUpdate(userId, { profilePicture: uploadResponse.secure_url }, { new: true }) // ✏️ Update and return the updated user
+        const updatedUser = await User.findByIdAndUpdate(userId, { profilePicture: uploadResponse.secure_url }, { new: true }) 
 
         res.status(200).json({ message: '✔️ Successfully updated user.', updatedUser })
     } catch (error) {
