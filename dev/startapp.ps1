62fb96e2fe7569201932cba96698f12be2b76541
khiem <khiemnguyen.hye@gmail.com>
2025-11-29 01:50:18 +0700

    Remove  development script.

--- dev/startapp.ps1
+++
@@ -1,5 +0,0 @@
-# Runs backend
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd backend; npm run dev"
-
-# Runs frontend
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd frontend; npm run dev"
