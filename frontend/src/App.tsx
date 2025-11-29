6a1eaf2a7b52c816aab4825a7a55672239a9b171
khiem <khiemnguyen.hye@gmail.com>
2025-11-29 10:11:45 +0700

    Format: Use single quotes.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,35 +11,35 @@ import { Loader } from 'lucide-react'
 import { Toaster } from 'react-hot-toast'
 
 const App = () => {
-    const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
 
-    useEffect(() => {
-        checkAuth()
-    }, [checkAuth])
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
 
-    console.log({ authUser })
+  console.log({ authUser })
 
-    if (isCheckingAuth && !authUser) {
-        return (
-            <div className="flex items-center justify-center h-screen">
-                <Loader className="size-10 animate-spin" />
-            </div>
-        )
-    }
-
+  if (isCheckingAuth && !authUser) {
     return (
-        <>
-            <NavBar />
-            <Routes>
-                <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-                <Route path="/signup" element={<SignupPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/settings" element={authUser ? <SettingPage /> : <Navigate to="/login" />} />
-            </Routes>
-            <Toaster />
-        </>
+      <div className='flex items-center justify-center h-screen'>
+        <Loader className='size-10 animate-spin' />
+      </div>
     )
+  }
+
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/settings' element={authUser ? <SettingPage /> : <Navigate to='/login' />} />
+      </Routes>
+      <Toaster />
+    </>
+  )
 }
 
 export default App
