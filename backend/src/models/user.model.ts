eec357bb30bf164dcc164c6c4522c0a2bc18003d
khiem <khiemnguyen.hye@gmail.com>
2025-11-29 11:31:24 +0700

    Reformat user schema.

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,27 +1,27 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema(
-    {
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-            minlength: 8,
-        },
-        avatar: {
-            type: String,
-            default: '',
-        },
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    { timestamps: true }
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    avatar: {
+      type: String,
+      default: '',
+    },
+  },
+  { timestamps: true }
 )
 
 const User = mongoose.model('User', userSchema)
